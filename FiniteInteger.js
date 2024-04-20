b2f1adf933c23128221bbb637f7a3200f7d623ee
Ariel Pettyjohn <me@arielpettyjohn.com>
2024-04-20 07:11:51 -0700

    Redesign and refactor to cover edge cases

--- FiniteInteger.js
+++ FiniteInteger.js
@@ -1,23 +1,34 @@
 class FiniteInteger {
-    static closedInterval (_minimum, _maximum = 0) {        
-        if (_maximum === _minimum) return [_minimum]; // degenerate interval
+    /*
+    static openInterval (infinum, supremum = 1) {
+        if (supremum === 1) {
+            return
+        }
+    }
+    */
 
+    static closedInterval (_minimum, _maximum = 0) {    
+        const isDegenerate = _maximum === _minimum;
+        if (isDegenerate) return [_minimum];
+        
+        const areArgumentsEmpty = arguments[0] === undefined;
+        const isConcise         = arguments[1] === undefined;
+        
         const isReversed = _minimum > _maximum;
+        const isEmpty    = areArgumentsEmpty || !isConcise && isReversed;
+        if (isEmpty) return [];        
         
-        const minimum    = isReversed ? _maximum : _minimum;
-        const maximum    = isReversed ? _minimum : _maximum;
+        const minimum = isConcise ? _maximum : _minimum;
+        const maximum = isConcise ? _minimum : _maximum;
         
-        const difference = maximum - minimum
-        
+        const difference = maximum - minimum;        
         const length     = difference <= 0 ? difference - 1 : difference + 1;
         const array      = [...Array(length).keys()];
-        
+
         const offsetInteger 
             = (integer) => integer + Math.sign(difference) * minimum;
         
-        return minimum === 0 
-            ? array 
-            : array.map(offsetInteger);
+        return minimum === 0 ? array : array.map(offsetInteger);
     }
 
     // Inclusive
@@ -28,4 +39,6 @@ class FiniteInteger {
     }
 }
 
-export default FiniteInteger;
+console.log(FiniteInteger.closedInterval(1))
+
+//export default FiniteInteger;
